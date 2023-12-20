a1c461bafd0a983b0a09171df7a36e2d4151885e
louisoduor <louisodhiambo20@gmail.com>
2023-12-20 21:03:58 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const drivers = [
+    "Bobby",
+    "Sammy",
+    "Sally",
+    "Annette",
+    "Sarah",
+    "bobby"
+  ];
+
+
+  function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
